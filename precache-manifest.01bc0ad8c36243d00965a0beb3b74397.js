bfdba731f05b7961d44b67a2be335ec398cb9e06
linahaljarad <60887416+linahaljarad@users.noreply.github.com>
2020-08-04 22:51:18 -0400

    Updates

---
+++ precache-manifest.01bc0ad8c36243d00965a0beb3b74397.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c5a05006235d46e44a95485051fbbf",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "c21de98d8180b8d8998e",
+    "url": "/to-do-list/static/css/main.31fd3c03.chunk.css"
+  },
+  {
+    "revision": "12425a2f0fba4a1a467d",
+    "url": "/to-do-list/static/js/2.748fdd47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.748fdd47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21de98d8180b8d8998e",
+    "url": "/to-do-list/static/js/main.318f90e7.chunk.js"
+  },
+  {
+    "revision": "b4db84ab7a0452a2a232",
+    "url": "/to-do-list/static/js/runtime-main.556ba592.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
